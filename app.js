d54c559c9411b1a880eddbea66ae2f23bada6b91
mxlisows <mxlisows@molsoncoors.com>
2019-10-10 20:37:36 +0200

    Static files configured

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));  // no longer needed thanks to staticfiles.config
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
