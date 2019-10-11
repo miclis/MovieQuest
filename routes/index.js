23004caac9663e101862ea0b9b95c33eb73fe02e
mxlisows <mxlisows@molsoncoors.com>
2019-10-11 22:11:12 +0200

    SessionId test

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Machine Learning Data Gathering Project',
-    description: 'Please help us gather data for our machine learning and optimization project :)'
+    description: 'Please help us gather data for our machine learning and optimization project :)',
+    id: req.sessionID // Id of the first movie
   });
 });
 
--- routes/movie.js
+++ routes/movie.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('movie', {
-    originalTitle: 'Mad Max Fury Road',
-    polishTitle: 'Mad Max Kolekcja',
-    year: ' (' + '2015' + ')',
-    director: 'Antoni Dupokrzakov vel Wilhelm von Kapustka',
-    genres: 'Action, Thriller, Post-Apo'
-  });
+/* GET movie page. */
+router.get('/:id', function(req, res, next) {
+    if (req.params.id >= 1 && req.params.id <=200) {
+        res.render('movie', {
+            originalTitle: 'Mad Max Fury Road',
+            polishTitle: 'Mad Max Kolekcja',
+            year: ' (' + '2015' + ')',
+            director: 'Antoni Dupokrzakov vel Wilhelm von Kapustka',
+            genres: ['Action', 'Thriller', 'Post-Apo']
+          });
+    } else {
+        let err = new Error('INVALID MOVIE ID');
+        err.status = 404;
+        throw err
+    }
+  
 });
 
 module.exports = router;
