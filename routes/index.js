651d49caeee87441ee295d15fae24691aadae06a
mxlisows <mxlisows@molsoncoors.com>
2019-10-14 21:56:32 +0200

    intro updated

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Machine Learning Data Gathering Project',
-    description: 'Please help us gather data for our machine learning and optimization project :)',
-    id: 1 + '?tmdbId=389',  // Id of first movie
-  });
+    res.render('index', {
+        title: 'Machine Learning Data Gathering Project',
+        description: `Please help us gather data for our machine learning and optimization project :)
+        We only ask you to simply press a button that represents your personal opinion regarding the movie.
+        If you haven't seen it - please skip to the next one.`,
+        id: 1 + '?tmdbId=389' // Id of first movie
+    });
 });
 
 module.exports = router;
