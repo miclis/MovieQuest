25f77f87499b1172facf3d9d314914680bc2aa16
mxlisows <mxlisows@molsoncoors.com>
2019-10-21 13:46:17 +0200

    Translation added.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ router.get('/', function(req, res, next) {
         title: 'Machine Learning Data Gathering Project',
         description: `Please help us gather data for our machine learning and optimization project :)
         We only ask you to simply press a button that represents your personal opinion regarding the movie.
-        If you haven't seen it - please skip to the next one.`,
+        If you haven't seen it - please click "NOT SEEN" button.`,
+        descriptionPL: `Pomóż nam zebrać dane do projektu z dziedziny Machine Learning i optymalizacji :)
+        Wystarczy, że naciśniesz przycisk odpowiadający Twojej ocenie danego filmu lub przycisk "NIE WIDZIAŁEM".`,
         id: 1 // Id of first movie
     });
 });
